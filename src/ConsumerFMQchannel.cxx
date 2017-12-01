201361abf5479ec69804f18570f2df6056148a2f
Sylvain Chapeland <o2.syc@cern.ch>
2017-12-01 09:07:53 +0100

    added unmanaged memory support

--- src/ConsumerFMQchannel.cxx
+++ src/ConsumerFMQchannel.cxx
@@ -1,4 +1,6 @@
 #include "Consumer.h"
+#include "ReadoutUtils.h"
+#include "MemoryHandler.h"
 
 
 #ifdef WITH_FAIRMQ
@@ -30,12 +32,21 @@ class ConsumerFMQchannel: public Consumer {
   private:
     std::unique_ptr<FairMQChannel> sendingChannel;
     std::shared_ptr<FairMQTransportFactory> transportFactory;   
-       
+    FairMQUnmanagedRegionPtr memoryBuffer=nullptr;
+    bool disableSending=0;
+    
   public: 
 
 
   ConsumerFMQchannel(ConfigFile &cfg, std::string cfgEntryPoint) : Consumer(cfg,cfgEntryPoint) {
 
+    int cfgDisableSending=0;
+    cfg.getOptionalValue<int>(cfgEntryPoint + ".disableSending", cfgDisableSending);
+    if (cfgDisableSending) {
+      theLog.log("FMQ message sending disabled");
+      disableSending=true;
+    }
+    
     std::string cfgTransportType="zeromq";
     cfg.getOptionalValue<std::string>(cfgEntryPoint + ".transportType", cfgTransportType);
     
@@ -52,6 +63,22 @@ class ConsumerFMQchannel: public Consumer {
             
     transportFactory=FairMQTransportFactory::CreateTransportFactory(cfgTransportType);
     sendingChannel=std::make_unique<FairMQChannel>(cfgChannelName, cfgChannelType, transportFactory);
+    
+    std::string cfgUnmanagedMemorySize="";
+    cfg.getOptionalValue<std::string>(cfgEntryPoint + ".unmanagedMemorySize",cfgUnmanagedMemorySize);
+    long long mMemorySize=ReadoutUtils::getNumberOfBytesFromString(cfgUnmanagedMemorySize.c_str());
+    if (mMemorySize>0) {
+      memoryBuffer=sendingChannel->Transport()->CreateUnmanagedRegion(mMemorySize);
+      theLog.log("Got FMQ unmanaged memory buffer size %lu @ %p",memoryBuffer->GetSize(),memoryBuffer->GetData());
+      std::unique_ptr<MemoryRegion> m;
+      m=std::make_unique<MemoryRegion>();
+      m->name="FMQ unmanaged memory buffer";
+      m->size=memoryBuffer->GetSize();
+      m->ptr=memoryBuffer->GetData();
+      bigBlock=std::move(m);
+    }
+    
+    
     sendingChannel->Bind(cfgChannelAddress);
     
     if (!sendingChannel->ValidateChannel()) {
@@ -61,16 +88,19 @@ class ConsumerFMQchannel: public Consumer {
   }
   
   ~ConsumerFMQchannel() {
+    bigBlock=nullptr;
   }
   
   int pushData(DataBlockContainerReference &b) {
 
+    if (disableSending) return 0;
+    
     // we create a copy of the reference, in a newly allocated object, so that reference is kept alive until this new object is destroyed in the cleanupCallback
     DataBlockContainerReference *ptr=new DataBlockContainerReference(b);
-    std::unique_ptr<FairMQMessage> msgHeader(transportFactory->CreateMessage((void *)&(b->getData()->header), (size_t)(b->getData()->header.headerSize), cleanupCallback, (void *)nullptr));
+    //std::unique_ptr<FairMQMessage> msgHeader(transportFactory->CreateMessage((void *)&(b->getData()->header), (size_t)(b->getData()->header.headerSize), cleanupCallback, (void *)nullptr));
     std::unique_ptr<FairMQMessage> msgBody(transportFactory->CreateMessage((void *)(b->getData()->data), (size_t)(b->getData()->header.dataSize), cleanupCallback, (void *)(ptr)));
 
-    sendingChannel->Send(msgHeader);
+    //sendingChannel->Send(msgHeader);
     sendingChannel->Send(msgBody);    
     return 0;
   }
