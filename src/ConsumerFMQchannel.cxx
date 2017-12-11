925058b65e63b002b7072e72c88c4f201e36d600
Sylvain Chapeland <o2.syc@cern.ch>
2017-12-11 15:02:14 +0100

    implementation of WP5 interface: one header per TF/data source + one msg per HBframe

--- src/ConsumerFMQchannel.cxx
+++ src/ConsumerFMQchannel.cxx
@@ -10,6 +10,9 @@
 #include <fairmq/FairMQTransportFactory.h>
 #include <fairmq/zeromq/FairMQTransportFactoryZMQ.h>
 
+#include "RAWDataHeader.h"
+#include "SubTimeframe.h"
+
 namespace {
 
 // cleanup function, defined with the callback footprint expected in the 3rd argument of FairMQTransportFactory.CreateMessage()
@@ -17,12 +20,20 @@ namespace {
 void cleanupCallback(void *data, void *object) {
     if ((object!=nullptr)&&(data!=nullptr)) {
       DataBlockContainerReference *ptr=(DataBlockContainerReference *)object;
-//      printf("ptr %p: use_count=%d\n",ptr,ptr->use_count());
+      //printf("release ptr %p: use_count=%d\n",ptr,ptr->use_count());
       delete ptr;
     }
   }
 
 }
+
+void cleanupCallbackForMalloc(void *data, void *object) {
+    if ((object!=nullptr)&&(data!=nullptr)) {
+      free(object);
+    }
+}
+
+
 
 /*
 TODO: add with/without state machine
@@ -91,17 +102,185 @@ class ConsumerFMQchannel: public Consumer {
     bigBlock=nullptr;
   }
   
-  int pushData(DataBlockContainerReference &b) {
+  int pushData(DataBlockContainerReference &) {
+    // this consumer does not accept a per-block push, it needs a set
+    return -1;
+  }
+  
+  int pushData(DataSetReference &bc) {
+  
+    if (disableSending) {
+      return 0;
+    }
 
-    if (disableSending) return 0;
+  
+    const unsigned int cruBlockSize=8192;
+    // todo: replace by RDHv3 length
+   
+    // we iterate a first time to count number of HB
+    SubTimeframe *stfHeader=(SubTimeframe *)malloc(sizeof(SubTimeframe));
+    if (stfHeader==nullptr) {return -1;}
+    stfHeader->timeframeId=0;
+    stfHeader->numberOfHBF=0;
+    stfHeader->linkId=0;
+
+    unsigned int lastHBid=-1;
+    int isFirst=true;
+    for (auto &br : *bc) {
+      DataBlock *b=br->getData();
+      if (isFirst) {
+        stfHeader->timeframeId=b->header.id;
+        stfHeader->linkId=b->header.linkId;
+        stfHeader->numberOfHBF=0;
+        isFirst=false;
+      } else {
+        if (stfHeader->timeframeId!=b->header.id) {printf("mismatch tfId\n");}
+        if (stfHeader->linkId!=b->header.linkId) {printf("mismatch linkId\n");}       
+      }
+      for (int offset=0;offset+cruBlockSize<=b->header.dataSize;offset+=cruBlockSize) {
+        //printf("checking %p : %d\n",b,offset);
+        o2::Header::RAWDataHeader *rdh=(o2::Header::RAWDataHeader *)&b->data[offset];       
+        if (rdh->heartbeatOrbit!=lastHBid) {
+          stfHeader->numberOfHBF++;
+          lastHBid=rdh->heartbeatOrbit;
+          //printf("offset %d now %d HBF - HBid=%d\n",offset,stfHeader->numberOfHBF,lastHBid);
+        }
+      }
+    }  
     
-    // we create a copy of the reference, in a newly allocated object, so that reference is kept alive until this new object is destroyed in the cleanupCallback
-    DataBlockContainerReference *ptr=new DataBlockContainerReference(b);
-    //std::unique_ptr<FairMQMessage> msgHeader(transportFactory->CreateMessage((void *)&(b->getData()->header), (size_t)(b->getData()->header.headerSize), cleanupCallback, (void *)nullptr));
-    std::unique_ptr<FairMQMessage> msgBody(transportFactory->CreateMessage((void *)(b->getData()->data), (size_t)(b->getData()->header.dataSize), cleanupCallback, (void *)(ptr)));
+    printf("TF %d link %d = %d blocks, %d HBf\n",(int)stfHeader->timeframeId,(int)stfHeader->linkId,(int)bc->size(),(int)stfHeader->numberOfHBF);
+    
 
+    // create a header message
+    std::unique_ptr<FairMQMessage> msgHeader(transportFactory->CreateMessage((void *)stfHeader, sizeof(SubTimeframe), cleanupCallbackForMalloc, (void *)(stfHeader)));
+    sendingChannel->Send(msgHeader);
+    //printf("sent header %d bytes\n",(int)sizeof(stfHeader));
+    
+    // cut: one message per HBf
+    lastHBid=-1;
+    
+    // this is for data not sent yet (from one loop to the next)
+    struct pendingFrame {
+     DataBlockContainerReference *blockRef;
+     unsigned int HBstart;
+     unsigned int HBlength;
+     unsigned int HBid;
+    };
+    std::vector<pendingFrame> pendingFrames;
+    
+    auto pendingFramesAppend = [&](unsigned int ix, unsigned int l, unsigned int id, DataBlockContainerReference br) {
+        pendingFrame pf;
+        pf.HBstart=ix;
+        pf.HBlength=l;
+        pf.HBid=id;
+        // we create a copy of the reference, in a newly allocated object, so that reference is kept alive until this new object is destroyed in the cleanupCallback
+        pf.blockRef=new DataBlockContainerReference(br);
+        //printf("allocating blockRef %p for %p\n",pf.blockRef,br);
+        pendingFrames.push_back(pf);       
+    };
+    
+    auto pendingFramesSend = [&]() {
+      int nFrames=pendingFrames.size();
+
+      if (nFrames==0) {
+        //printf("no pending frames\n");
+        return;
+      }
+           
+      if (nFrames==1) {
+        // single block, no need to repack
+        auto br=*(pendingFrames[0].blockRef);
+        DataBlock *b=br->getData();
+        int ix=pendingFrames[0].HBstart;
+        int l=pendingFrames[0].HBlength;
+        std::unique_ptr<FairMQMessage> msgBody(transportFactory->CreateMessage((void *)(&(b->data[ix])),(size_t)(l), cleanupCallback, (void *)(pendingFrames[0].blockRef)));
+        sendingChannel->Send(msgBody);
+        //printf("block %p @ %d : %d\n",b,ix,l);
+        //printf("sent single HB %d = %d bytes\n",pendingFrames[0].HBid,l);
+        //printf("left to FMQ: %p\n",pendingFrames[0].blockRef);
+
+      } else {
+        // multiple blocks, need to repack
+        int totalSize=0;
+        for (auto &f : pendingFrames) {
+          totalSize+=f.HBlength;
+        }
+        char *newBlock=(char *)malloc(totalSize);
+        int newIx=0;
+        for (auto &f : pendingFrames) {
+          auto br=*(f.blockRef);
+          DataBlock *b=br->getData();
+          int ix=f.HBstart;
+          int l=f.HBlength;
+          //printf("block %p @ %d : %d\n",b,ix,l);
+          memcpy(&newBlock[newIx],&(b->data[ix]),l);
+          //printf("release %p for %p\n",f.blockRef,br);
+          delete f.blockRef;
+          f.blockRef=nullptr;
+          newIx+=l;
+        }
+        std::unique_ptr<FairMQMessage> msgBody(transportFactory->CreateMessage((void *)newBlock, totalSize, cleanupCallbackForMalloc, (void *)(newBlock)));
+        sendingChannel->Send(msgBody);
+        //printf("sent reallocated HB %d (originally %d blocks) = %d bytes\n",pendingFrames[0].HBid,nFrames,totalSize);
+
+      }
+      pendingFrames.clear();
+    };
+    
+    
+    
+    for (auto &br : *bc) {
+      DataBlock *b=br->getData();
+      unsigned int HBstart=0;
+      for (int offset=0;offset+cruBlockSize<=b->header.dataSize;offset+=cruBlockSize) {
+        o2::Header::RAWDataHeader *rdh=(o2::Header::RAWDataHeader *)&b->data[offset];       
+        //printf("CRU block %p = HB %d link %d @ %d\n",b,(int)rdh->heartbeatOrbit,(int)rdh->linkId,offset);
+        if (rdh->heartbeatOrbit!=lastHBid) {
+          //printf("new HBf detected\n");
+          int HBlength=offset-HBstart;
+          
+          if (HBlength) {
+            // add previous block to pending frames
+            pendingFramesAppend(HBstart,HBlength,lastHBid,br);
+          }
+          // send pending frames, if any
+          pendingFramesSend();
+
+          // update new HB frame
+          HBstart=offset;
+          lastHBid=rdh->heartbeatOrbit;
+        }
+      }
+      
+      // keep last piece for later, HBframe may continue in next block(s)
+      if (HBstart<b->header.dataSize) {
+        pendingFramesAppend(HBstart,b->header.dataSize-HBstart,lastHBid,br);
+      }
+    }
+    
+    // purge pendingFrames
+    pendingFramesSend();
+    
+    /*
+
+      std::unique_ptr<FairMQMessage> msgBody(transportFactory->CreateMessage((void *)(&b->data[HBstart]), (size_t)(HBlength), cleanupCallback, (void *)(ptr)));
+      sendingChannel->Send(msgBody);
+      printf("HB %d link %d @ %d + %d\n",lastHBid,(int)rdh->linkId,HBstart,HBlength);
+      printf("(last part) sent %d bytes\n",(int)HBlength);
+*/
+      
+      // todo: wait until next block to check if HBF continues on top... if so, realloc into a single block
+          
+    
+    
+/*    // we create a copy of the reference, in a newly allocated object, so that reference is kept alive until this new object is destroyed in the cleanupCallback
+    DataBlockContainerReference *ptr=new DataBlockContainerReference(br);
+    //std::unique_ptr<FairMQMessage> msgHeader(transportFactory->CreateMessage((void *)&(b->getData()->header), (size_t)(b->getData()->header.headerSize), cleanupCallback, (void *)nullptr));
+    std::unique_ptr<FairMQMessage> msgBody(transportFactory->CreateMessage((void *)(br->getData()->data), (size_t)(br->getData()->header.dataSize), cleanupCallback, (void *)(ptr)));
     //sendingChannel->Send(msgHeader);
     sendingChannel->Send(msgBody);    
+    return 0;
+    */
     return 0;
   }
   
