cef3447bd1cf40bfeda825c5bc01c9d2a2db17e9
Sylvain Chapeland <o2.syc@cern.ch>
2017-12-20 09:18:41 +0100

    simple fmq msg recv program

---
+++ src/testRxFMQ.cxx
@@ -0,0 +1,33 @@
+#include <fairmq/FairMQDevice.h>
+#include <fairmq/FairMQMessage.h>
+#include <fairmq/FairMQTransportFactory.h>
+#include <fairmq/zeromq/FairMQTransportFactoryZMQ.h>
+#include <memory>
+
+int main() {
+
+  std::string cfgTransportType="shmem";
+  std::string cfgChannelName="test";
+  std::string cfgChannelType="pair";
+  std::string cfgChannelAddress="ipc:///tmp/test-pipe";
+
+  auto factory = FairMQTransportFactory::CreateTransportFactory(cfgTransportType);
+  auto pull = FairMQChannel{cfgChannelName, cfgChannelType, factory};
+  pull.Connect(cfgChannelAddress);  
+
+  for (;;) {
+    auto msg = pull.NewMessage();
+    if (pull.ReceiveAsync(msg)>0) {
+      if (msg->GetSize()==0) {continue;}
+      int sz=(int)msg->GetSize();
+      void *data=msg->GetData();
+      printf("Received message %p size %d\n",data,sz);
+      sleep(1);
+      printf("Releasing message %p size %d\n",data,sz);
+    } else {
+      usleep(1000);
+    }
+  }
+
+  return 0;
+}
