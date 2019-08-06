4e3d7c51be01cb3891a920660c84b1be9312b334
Sylvain Chapeland <o2.syc@cern.ch>
2019-08-06 14:26:40 +0200

    added copyright header

--- src/MemoryPagesPool.cxx
+++ src/MemoryPagesPool.cxx
@@ -1,3 +1,13 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 #include "MemoryPagesPool.h"
 
 MemoryPagesPool::MemoryPagesPool(size_t vPageSize, size_t vNumberOfPages, void *vBaseAddress, size_t vBaseSize,  ReleaseCallback vCallback, size_t firstPageOffset) {
--- src/SubTimeframe.h
+++ src/SubTimeframe.h
@@ -1,3 +1,13 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 // definition of a header message for a subtimeframe
 // subtimeframe made of 1 message with this header
 // followed by 1 message for each heartbeat-frame
