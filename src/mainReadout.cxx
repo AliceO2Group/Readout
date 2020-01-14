b16bb461202e76cb25a20526fb3764283b6387a0
Sylvain Chapeland <o2.syc@cern.ch>
2020-01-14 16:38:41 +0100

    added mode with interactive state change

--- src/mainReadout.cxx
+++ src/mainReadout.cxx
@@ -50,6 +50,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <termios.h>
 
 #ifdef WITH_NUMA
 #include <numa.h>
@@ -80,6 +81,45 @@
 // namespace used
 using namespace AliceO2::InfoLogger;
 using namespace AliceO2::Common;
+
+// set log environment before theLog is initialized
+class TtyChecker {
+  private:
+    bool isInteractive = false;
+    struct termios initial_settings;
+    
+  public:
+  TtyChecker() {
+    // if launched from terminal, force logs to terminal
+    if (isatty(fileno(stdin))) {
+      if (getenv("INFOLOGGER_MODE")==nullptr) {
+        printf("Launching from terminal, logging here\n");
+        setenv("INFOLOGGER_MODE","stdout",1);
+      }	
+      isInteractive = true;
+    }
+    if (isInteractive) {
+      // set non-blocking input
+      struct termios new_settings;
+      tcgetattr(0,&initial_settings);
+      new_settings = initial_settings;
+      new_settings.c_lflag &= ~ICANON;
+      new_settings.c_lflag &= ~ECHO;
+      // do not disable ctrl+c signal
+      //new_settings.c_lflag &= ~ISIG;
+      new_settings.c_cc[VMIN] = 0;
+      new_settings.c_cc[VTIME] = 0;
+      tcsetattr(0, TCSANOW, &new_settings);
+    }
+  };
+  ~TtyChecker() {
+    if (isInteractive) {
+      // restore term settings
+      tcsetattr(0, TCSANOW, &initial_settings);
+    }
+  };  
+};
+TtyChecker theTtyChecker;
 
 // global entry point to log system
 InfoLogger theLog;
@@ -914,6 +954,15 @@ int Readout::stop() {
 
   // ensure output buffers empty ?
   
+  // check status of memory pools
+  for (auto &&readoutDevice : readoutDevices) {
+    size_t nPagesTotal=0, nPagesFree=0, nPagesUsed=0;
+    if (readoutDevice->getMemoryUsage(nPagesFree, nPagesTotal)==0) {
+      nPagesUsed = nPagesTotal-nPagesFree;
+      theLog.log("Equipment %s : %d/%d pages (%.2f%%) still in use", readoutDevice->getName().c_str(), nPagesUsed, nPagesTotal, nPagesUsed*100.0/nPagesTotal);
+    }
+  }
+  
   // publish final logbook statistics
   publishLogbookStats();
   gReadoutStats.reset();
@@ -1090,6 +1139,13 @@ int main(int argc, char *argv[]) {
     occMode = true;
   }
 
+  // flag to run readout states interactively from console
+  bool interactiveMode = false;
+  if (getenv("O2_READOUT_INTERACTIVE") != nullptr) {
+    interactiveMode = true;
+    occMode = false;
+  }
+
   // initialize logging
   theLogContext.setField(InfoLoggerContext::FieldName::Facility,"readout");
   theLog.setContext(theLogContext);
@@ -1115,6 +1171,169 @@ int main(int argc, char *argv[]) {
                "OCC mode requested but not available in this build");
     return -1;
 #endif
+  } else if (interactiveMode) {
+    theLog.log("Readout entering interactive state machine");
+    theLog.log("(c) configure (s) start (t) stop (r) reset (r) recover (x) quit");
+       
+    enum class States {Undefined, Standby, Configured, Running, Error};
+    enum class Commands {Undefined, Configure, Reset, Start, Stop, Recover, Exit};
+    
+    auto getStateName = [] (States s) {
+      switch(s) {
+        case States::Standby:
+	  return "standby";
+	case States::Configured:
+	  return "configured";
+	case States::Running:
+	  return "running";
+	case States::Error:
+	  return "error";	
+      }
+      return "undefined";
+    };
+    
+    auto getCommandName = [] (Commands c) {
+      switch(c) {
+        case Commands::Configure:
+	  return "configure";
+        case Commands::Start:
+	  return "start";
+        case Commands::Stop:
+	  return "stop";
+        case Commands::Reset:
+	  return "reset";
+        case Commands::Recover:
+	  return "recover";
+        case Commands::Exit:
+	  return "exit";
+      }
+      return "undefined";
+    };
+    
+    States theState = States::Standby;
+    Commands theCommand = Commands::Undefined;
+    printf("State: %s\n",getStateName(theState));
+    for(;;) {
+      if (theCommand == Commands::Undefined) {
+        int c=getchar();
+	if (c>0) {        
+	  switch(c) {
+	    case 'c':
+	      theCommand = Commands::Configure;
+	      break;
+	    case 's':
+	      theCommand = Commands::Start;
+	      break;
+	    case 't':
+	      theCommand = Commands::Stop;
+	      break;
+	    case 'r':
+	      theCommand = Commands::Reset;
+	      break;
+	    case 'v':
+	      theCommand = Commands::Recover;
+	      break;
+	    case 'x':
+	      theCommand = Commands::Exit;
+	      break;
+	  }
+	}
+      }	
+
+      if (theCommand != Commands::Undefined) {
+	printf("Executing %s\n",getCommandName(theCommand));
+      }
+	
+      States newState = States::Undefined;	
+      bool isCommandValid = true;
+      if (theState == States::Standby) {
+	if (theCommand == Commands::Configure) {	  
+          boost::property_tree::ptree properties; // an empty "extra" config
+          err = theReadout->configure(properties);
+          if (err) {
+            newState = States::Error;
+          } else {
+	    newState = States::Configured;
+	  }
+	} else {
+          isCommandValid = false;
+	}
+      } else if (theState == States::Configured) {
+	if (theCommand == Commands::Start) {
+	  err = theReadout->start();
+	  if (err) {
+            newState = States::Error;
+	  } else {
+            newState = States::Running;
+	  }
+	} else if (theCommand == Commands::Reset) {
+	  err = theReadout->reset();
+	  if (err) {
+	    newState = States::Error;
+	  } else {
+	    newState = States::Standby;
+	  }
+	} else {
+          isCommandValid = false;
+	}
+      } else if (theState == States::Running) {
+	if (theCommand == Commands::Stop) {
+	  err = theReadout->stop();
+	  if (err) {
+            newState = States::Error;
+	  } else {
+            newState = States::Configured;
+	  }
+	} else {
+          isCommandValid = false;
+	}
+      } else if (theState == States::Error) {
+	if (theCommand == Commands::Reset) {
+	  err = theReadout->reset();
+	  if (err) {
+            newState = States::Error;
+	  } else {
+            newState = States::Standby;
+	  }
+	} else {
+          isCommandValid = false;
+	}
+      }
+
+      if (theCommand == Commands::Exit) {
+	break;
+      }
+      
+      if (newState != States::Undefined) {
+        printf("State: %s\n",getStateName(theState));
+	theState = newState;
+      }
+      if ((theCommand != Commands::Undefined) && (!isCommandValid)) {
+	printf("This command is invalid in current state\n");
+      }
+
+      theCommand = Commands::Undefined;
+
+      if (theState == States::Running) {
+        err = theReadout->iterateRunning();
+        if (err == 1) {
+          theLog.log("Readout requesting to stop");
+          theCommand = Commands::Stop;
+        } else if (err != 0) {
+          theLog.log(InfoLogger::Severity::Error,
+                     "Readout reported an error while running");
+          theCommand = Commands::Stop;
+        }
+        err = theReadout->iterateCheck();
+        if (err) {
+          theLog.log(InfoLogger::Severity::Error, "Readout reported an error");
+          theCommand = Commands::Stop;
+        }
+      } else {
+        usleep(100000);
+      }          
+    }
+    
   } else {
     theLog.log("Readout entering standalone state machine");
     boost::property_tree::ptree properties; // an empty "extra" config
