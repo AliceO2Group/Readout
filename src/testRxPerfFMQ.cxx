722349e1b6e4d47fec03608056979bd6d6c27e5a
Sylvain Chapeland <o2.syc@cern.ch>
2020-03-13 17:02:31 +0100

    added test util for FMQ Tx/Rx perf

---
+++ src/testRxPerfFMQ.cxx
@@ -0,0 +1,124 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+// test receiver program to benchmark FMQ interprocess communication
+
+#include <Common/Timer.h>
+#include <fairmq/FairMQDevice.h>
+#include <fairmq/FairMQMessage.h>
+#include <fairmq/FairMQTransportFactory.h>
+#include <memory>
+#include <stdio.h>
+#include <vector>
+
+#include "ReadoutUtils.h"
+
+int main() {
+
+  std::string cfgTransportType = "shmem";
+  std::string cfgChannelName = "test";
+  std::string cfgChannelType = "pair";
+  std::string cfgChannelAddress = "ipc:///tmp/test-pipe";
+
+  auto factory =
+      FairMQTransportFactory::CreateTransportFactory(cfgTransportType);
+  auto pull = FairMQChannel{cfgChannelName, cfgChannelType, factory};
+  pull.Connect(cfgChannelAddress);
+  int64_t ret;
+
+  printf("Starting FMQ multi-part receiver '%s' %s %s @ %s\n",
+         cfgChannelName.c_str(), cfgTransportType.c_str(),
+         cfgChannelType.c_str(), cfgChannelAddress.c_str());
+
+  int statInterval = 1; // interval between stats, in seconds
+  AliceO2::Common::Timer timerStats;
+  AliceO2::Common::Timer runningTime;
+  timerStats.reset(1000000.0 * statInterval);
+
+  size_t msgCount = 0;
+  size_t msgCountPart = 0;
+  size_t msgNew = 0;
+  size_t msgNewPart = 0;
+  double tNew = 0.0;
+  double utimeNew = 0.0;
+  double stimeNew = 0.0;
+  bool isFirstStat = 1;
+  bool isMultiPart = 1;
+
+  double txCPU = 0;
+
+  for (;;) {
+    std::vector<FairMQMessagePtr> msgs;
+    FairMQParts msgParts;
+
+    if (isMultiPart) {
+      ret = pull.Receive(msgParts, statInterval * 1000);
+    } else {
+      ret = pull.Receive(msgs, statInterval * 1000);
+    }
+
+    if (timerStats.isTimeout()) {
+      double t = timerStats.getTime();
+      double tt = runningTime.getTime();
+      double uTime, sTime;
+      double uTimePercent = -1, sTimePercent = -1;
+      if (getProcessStats(uTime, sTime) == 0) {
+        uTimePercent = (uTime - utimeNew) * 100.0 / t;
+        sTimePercent = (sTime - stimeNew) * 100.0 / t;
+        stimeNew = sTime;
+        utimeNew = uTime;
+      }
+      if (isFirstStat) {
+        isFirstStat = 0;
+        printf("Interval   Messages       Rate      Parts       Rate     "
+               "------- Rx CPU used --------      Tx CPU    Total\n");
+        printf("       s        msg         Hz        msg         Hz     total "
+               "%%   user %%  system %%             %%      msg\n");
+      }
+      printf("%8.1f   %8lu   %8.1lf   %8lu   %8.1lf    %8.1lf %8.1lf  %8.1lf   "
+             "     %6.0lf %8lu\n",
+             t, msgNew, msgNew / t, msgNewPart, msgNewPart / t,
+             uTimePercent + sTimePercent, uTimePercent, sTimePercent, txCPU,
+             msgCount);
+      msgNew = 0;
+      msgNewPart = 0;
+      while (timerStats.isTimeout()) {
+        timerStats.increment();
+      }
+    }
+
+    if (ret > 0) {
+      if (isMultiPart) {
+        msgCount++;
+        msgNew++;
+        int nPart = msgParts.Size();
+        msgCountPart += nPart;
+        msgNewPart += nPart;
+        for (auto const &mm : msgParts) {
+          // bad trick: TX CPU used (in previous second) encoded in message size
+          txCPU = mm->GetSize() - 100;
+          break;
+        }
+
+      } else {
+        for (auto &msg : msgs) {
+          int sz = (int)msg->GetSize();
+          void *data = msg->GetData();
+          // printf("Received message %p size %d\n", data, sz);
+          // printf("Releasing message %p size %d\n", data, sz);
+          msgCount++;
+          msgNew++;
+        }
+      }
+    }
+  }
+
+  return 0;
+}
---
+++ src/testTxPerfFMQ.cxx
@@ -0,0 +1,127 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+// test sender program to benchmark FMQ interprocess communication
+
+#include <Common/Timer.h>
+#include <fairmq/FairMQDevice.h>
+#include <fairmq/FairMQMessage.h>
+#include <fairmq/FairMQTransportFactory.h>
+#include <memory>
+#include <stdio.h>
+
+#include "ReadoutUtils.h"
+
+int main() {
+
+  std::string cfgTransportType = "shmem";
+  std::string cfgChannelName = "test";
+  std::string cfgChannelType = "pair";
+  std::string cfgChannelAddress = "ipc:///tmp/test-pipe";
+
+  auto transportFactory =
+      FairMQTransportFactory::CreateTransportFactory(cfgTransportType);
+  auto channel =
+      FairMQChannel{cfgChannelName, cfgChannelType, transportFactory};
+  channel.Bind(cfgChannelAddress);
+  if (!channel.Validate()) {
+    return -1;
+  }
+
+  const size_t bufferSize = 2000 * 1024L * 1024L;
+  auto memoryBuffer = channel.Transport()->CreateUnmanagedRegion(
+      bufferSize, [](void *data, size_t size, void *hint) {
+        // cleanup callback
+      });
+  printf("Created buffer %p size %ld\n", memoryBuffer->GetData(),
+         memoryBuffer->GetSize());
+
+  int statInterval = 1; // interval between stats, in seconds
+  AliceO2::Common::Timer runningTime;
+  AliceO2::Common::Timer timerStats;
+
+  size_t msgCount = 0;
+  size_t msgMax = 10000;
+  size_t msgSize = 100;
+
+  size_t msgParts = 257;
+  double msgRate = 3168;
+  size_t sequenceTime = 15; // duration of each sequence
+
+  char *buf = (char *)memoryBuffer->GetData();
+  size_t ix = 0;
+
+  double lastCPUu = 0;
+  double lastCPUs = 0;
+  double CPUt = 0;
+
+  /*
+    for (msgRate=10; msgRate<=1000000; msgRate*=10) {
+    for (msgParts=1;msgParts<=1024;msgParts*=4) {
+  */
+  msgMax = msgRate * sequenceTime;
+
+  msgCount = 0;
+  runningTime.reset();
+  timerStats.reset(1000000.0 * statInterval);
+
+  getProcessStats(lastCPUu, lastCPUs);
+  std::vector<FairMQMessagePtr> msgs;
+  msgs.reserve(msgParts);
+
+  printf(
+      "starting sequence for %lus : rate = %.0lfHz, %lu parts per message,\n",
+      sequenceTime, msgRate, msgParts);
+  for (size_t i = 0; i < msgMax; i++) {
+    msgs.clear();
+
+    for (size_t im = 0; im < msgParts; im++) {
+      msgSize = 100 + (size_t)CPUt;
+      void *dataPtr = (void *)(&buf[ix]);
+      void *hint = (void *)i;
+      ix += msgSize;
+      if (ix >= bufferSize) {
+        ix = 0;
+      }
+      msgs.emplace_back(
+          std::move(channel.NewMessage(memoryBuffer, dataPtr, msgSize, hint)));
+    }
+    channel.Send(msgs);
+    msgCount++;
+
+    for (;;) {
+      double now = runningTime.getTime();
+      double rate = msgCount / runningTime.getTime();
+      if (rate <= msgRate) {
+        break;
+      }
+      usleep(1000000 / msgRate);
+    }
+
+    if (timerStats.isTimeout()) {
+      double CPUu, CPUs;
+      getProcessStats(CPUu, CPUs);
+      CPUt = (CPUu - lastCPUu + CPUs - lastCPUs) * 100.0 / timerStats.getTime();
+      lastCPUu = CPUu;
+      lastCPUs = CPUs;
+
+      printf("%lu -> CPU = %lf %%\n", msgCount, CPUt);
+      timerStats.increment();
+    }
+  }
+  printf("sequence completed\n");
+  sleep(3);
+  /*
+    }
+    }
+  */
+
+  return 0;
+}
