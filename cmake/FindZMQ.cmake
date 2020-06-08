a58110d57e59d5dfc816468ed0e11658f5dedddc
Sylvain Chapeland <o2.syc@cern.ch>
2020-06-08 17:48:00 +0200

    renamed cmake find ZMQ

---
+++ cmake/FindZMQ.cmake
@@ -0,0 +1,37 @@
+# Try to find the ZeroMQ package include dirs and libraries
+# Author: Sylvain Chapeland
+#
+# This script will set the following variables:
+#  ZMQ_FOUND - System has ZeroMQ
+#  ZMQ_INCLUDE_DIRS - The ZeroMQ include directories
+#  ZMQ_LIBRARIES - The libraries needed to use ZeroMQ
+#
+# This script can use the following variables:
+#  ZMQ_ROOT - Installation root to tell this module where to look.
+
+find_path(
+  ZMQ_INCLUDE_DIRS NAMES zmq.h
+  PATHS ${ZMQ_ROOT} ${ZMQ_ROOT}/include
+  NO_DEFAULT_PATH)
+  
+find_library(
+  ZMQ_LIBRARIES NAMES zmq
+  PATHS ${ZMQ_ROOT} ${ZMQ_ROOT}/lib
+  NO_DEFAULT_PATH)
+ 
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ZMQ
+  DEFAULT_MSG
+  ZMQ_INCLUDE_DIRS
+  ZMQ_LIBRARIES
+)
+
+if(${ZMQ_FOUND})
+    message(
+    STATUS
+    "Found ZMQ using path = ${ZMQ_ROOT} (include: ${ZMQ_INCLUDE_DIRS} library: ${ZMQ_LIBRARIES})")    
+endif()
+
+list(APPEND ZMQ_LIBRARIES pthread)
+
+mark_as_advanced(ZMQ_INCLUDE_DIRS ZMQ_LIBRARIES)
