44b2ad8bbdf3f9d1fb5472dc81720707c65e688b
Sylvain Chapeland <o2.syc@cern.ch>
2020-06-15 10:51:16 +0200

    reshuffled struct and added fields for new stf format

--- src/DataBlock.h
+++ src/DataBlock.h
@@ -24,24 +24,41 @@ typedef enum {
 /// Definition of a unique identifier for blocks
 typedef uint64_t DataBlockId;
 
+/// Definition of default values
+const uint64_t undefinedBlockId = 0;        ///< default value, when blockId undefined
+const uint64_t undefinedTimeframeId = 0;    ///< default value, when timeframeId undefined
+const uint8_t undefinedSystemId = 0xFF;      ///< default value, when linkId undefined
+const uint16_t undefinedFeeId = 0xFFFF;      ///< default value, when linkId undefined
+const uint16_t undefinedEquipmentId = 0xFFFF; ///< default value, when equipmentId undefined
+const uint8_t undefinedLinkId = 0xFF;      ///< default value, when linkId undefined
+const uint32_t undefinedOrbit = 0;          ///< default value, when orbit undefined
+
+
 /// Data structure is based on standard data types with specified width.
 /// headerPtr + headerSize = payloadPtr
 /// headerPtr + headerSize + dataSize = nextHeaderPtr (if not toplevel block header)
 typedef struct {
-  uint32_t blockType;   ///< ID to identify structure type
-  uint32_t headerSize;  ///< header size in bytes
-  uint32_t dataSize;    ///< data size following this structure (until next header, if this is not a toplevel block header)
-  DataBlockId blockId;  ///< id of the block (strictly monotonic increasing sequence)
-  uint32_t linkId;      ///< id of link
-  uint16_t equipmentId; ///< id of equipment generating the data
-  uint64_t timeframeId; ///< id of timeframe
-  DataBlockId pipelineId;  ///< id used to sort data in/out in parallel pipelines
-} DataBlockHeaderBase;
+  uint32_t blockType;     ///< ID to identify structure type
+  uint32_t headerSize;    ///< header size in bytes
+  uint32_t dataSize;      ///< data size following this structure (until next header, if this is not a toplevel block header)
+  union {
+    uint32_t reserved[61];
+    struct {
+      DataBlockId blockId = undefinedBlockId;       ///< id of the block (strictly monotonic increasing sequence)    
+      DataBlockId pipelineId = undefinedBlockId;    ///< id used to sort data in/out in parallel pipelines
+            
+      uint64_t timeframeId = undefinedTimeframeId;  ///< id of timeframe
+      uint8_t systemId = undefinedSystemId;        /// from RDH
+      uint16_t feeId = undefinedFeeId;             /// from RDH
+      uint16_t equipmentId = undefinedEquipmentId; /// id of equipment generating the data
+      uint8_t linkId = undefinedLinkId;            /// from RDH
+      uint32_t timeframeOrbitFirst = undefinedOrbit; /// from timeframe
+      uint32_t timeframeOrbitLast = undefinedOrbit;  /// from timeframe
+      bool flagEndOfTimeframe = 0 ; ///< flag to signal this is the last TF block
+    };
+  };
 
-const uint64_t undefinedBlockId = 0;        ///< default value, when blockId undefined
-const uint32_t undefinedLinkId = 0xFF;      ///< default value, when linkId undefined
-const uint16_t undefinedEquipmentId = 0xFF; ///< default value, when equipmentId undefined
-const uint64_t undefinedTimeframeId = 0;    ///< default value, when timeframeId undefined
+} DataBlockHeaderBase;
 
 /// Add extra types below, e.g.
 ///
