e40b823e044d4fd5b5dca4cd62b33bdbf2c3b52e
Sylvain Chapeland <o2.syc@cern.ch>
2020-06-22 15:00:01 +0200

    RDH systemId

--- src/RdhUtils.h
+++ src/RdhUtils.h
@@ -44,6 +44,7 @@ public:
   // access RDH fields
   // functions defined inline here
   inline uint8_t getHeaderVersion() { return rdhPtr->version; }
+  inline uint8_t getSystemId() { return (uint8_t)rdhPtr->systemId; }
   inline uint16_t getFeeId() { return (uint16_t)rdhPtr->feeId; }
   inline uint8_t getLinkId() { return (uint8_t)rdhPtr->linkId; }
   inline uint8_t getPacketCounter() { return (uint8_t)rdhPtr->packetCounter; }
