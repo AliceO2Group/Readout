2a168021d09f66c19bede5580f86a8848e5f5ee7
Sylvain Chapeland <o2.syc@cern.ch>
2020-06-25 15:02:04 +0200

    cleanup defaults

--- src/SubTimeframe.h
+++ src/SubTimeframe.h
@@ -18,7 +18,7 @@ struct SubTimeframe {
   uint64_t timeframeId = 0; // id of timeframe
   uint8_t systemId = 0xFF;
   uint16_t feeId = 0xFFFF;
-  uint16_t equipmentId = 0xFF;
+  uint16_t equipmentId = 0xFFFF;
   uint8_t linkId = 0xFF;
   uint32_t timeframeOrbitFirst = 0;
   uint32_t timeframeOrbitLast = 0;
@@ -26,7 +26,7 @@ struct SubTimeframe {
     uint8_t flags = 0;
     struct {
       uint8_t lastTFMessage : 1; // bit 0
-      uint8_t isRdhFormat : 1; // bit 1
+      uint8_t isRdhFormat : 1;   // bit 1
       uint8_t flagsUnused : 6;   // bit 2-7: unused
     };
   };
