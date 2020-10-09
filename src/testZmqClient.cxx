28f7db5819018d75a63193394992d8178440cb9c
Sylvain Chapeland <o2.syc@cern.ch>
2020-10-09 15:06:11 +0200

    extra stats and auto exit

--- src/testZmqClient.cxx
+++ src/testZmqClient.cxx
@@ -1,8 +1,16 @@
 #include "ZmqClient.hxx"
 #include <unistd.h>
 
+unsigned long long totalBytes = 0;
+unsigned long long intervalBytes = 0;
+
 int callback(void *msg, int msgSize) {
+//  printf("Block = %d\n",msgSize);  
+  totalBytes += msgSize;
+  intervalBytes += msgSize;
+  return 0;
   uint64_t tf;
+  
   if (msgSize==sizeof(tf)) {
     printf("TF %lu\n",*((uint64_t *)msg));
     return 0;
@@ -10,12 +18,30 @@ int callback(void *msg, int msgSize) {
   return -1;
 }
 
-int main() {
+int main(int argc, char **argv) {
+  int id=0;
+  
+  if (argc>=2) {id=atoi(argv[1]);}
+  
+  int nNoData=999;
+  
   try {
-    ZmqClient c;
-    c.setCallback(callback);
-    
-    sleep(5);
+    ZmqClient c("ipc:///tmp/ctp-readout");
+    for (;;) {
+    c.setCallback(callback);    
+    sleep(1);
+    printf("%d\t%.2f Gb/s\t%.02fMB\n", id, intervalBytes*8.0/(1024.0*1024.0*1024.0), totalBytes/(1024.0*1024.0));
+    if (intervalBytes == 0) {
+      nNoData++;
+      if (nNoData==5) {
+        printf("Bytes total = %llu bytes\n",totalBytes);
+	break;
+      }
+    } else {
+      nNoData=0;
+    }
+    intervalBytes = 0;    
+    }
   }
   catch(...) {
   }
