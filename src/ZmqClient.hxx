d872fce0d511b1cb681dc313f85ce49b3f062332
Sylvain Chapeland <o2.syc@cern.ch>
2020-11-10 18:22:16 +0100

    pause, buffer, cleanup

--- src/ZmqClient.hxx
+++ src/ZmqClient.hxx
@@ -7,17 +7,22 @@
 class ZmqClient
 {
  public:
-  ZmqClient(const std::string& url = "tcp://127.0.0.1:50001");
+  ZmqClient(const std::string& url = "tcp://127.0.0.1:50001", const int maxMsgSize = 1024L*1024L);
   ~ZmqClient();
 
   int setCallback(std::function<int(void* msg, int msgSize)>);
-
+  void setPause(int); // to pause/unpause data RX
+  
  private:
   std::string cfgAddress;
+  int cfgMaxMsgSize;  
 
   void* context = nullptr;
   void* zh = nullptr;
+  void* msgBuffer = nullptr;
 
+  bool isPaused = false;
+  
   std::unique_ptr<std::thread> th;  // thread receiving data
   std::atomic<int> shutdownRequest; // flag to be set to 1 to stop thread
   void run();                       // code executed in separate thread
