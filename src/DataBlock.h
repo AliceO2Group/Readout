bfe6dc9b8589604352d24a242036df84d7e91031
Sylvain Chapeland <o2.syc@cern.ch>
2021-07-23 12:10:36 +0200

    updated o2 copyright

--- src/DataBlock.h
+++ src/DataBlock.h
@@ -1,3 +1,14 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 /// \file DataBlock.h
 ///
 /// This defines the POD structures used in readout to organize data in memory.
@@ -77,3 +88,4 @@ static_assert(std::is_pod<DataBlockHeader>::value, "DataBlockHeader is not a POD
 static_assert(std::is_pod<DataBlock>::value, "DataBlock is not a POD");
 
 #endif /* READOUT_DATABLOCK */
+
--- src/MemoryPagesPool.cxx
+++ src/MemoryPagesPool.cxx
@@ -1,8 +1,9 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -267,3 +268,4 @@ size_t MemoryPagesPool::getDataBlockMaxSize() { return pageSize - headerReserved
 std::string MemoryPagesPool::getStats() {
   return "number of pages used: " + std::to_string(poolStats.getTotal()) + " average free pages: " + std::to_string((uint64_t)poolStats.getAverage()) + " minimum free pages: " + std::to_string(poolStats.getMinimum());
 }
+
